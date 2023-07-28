44389e6a71f667d873a8ed6c77aac1d45805ff99
FILPRIDE <FILPRIDE@DESKTOP-LCC7IC5>
2023-07-28 13:41:58 +0800

    Adding Public Function for hashing the password

--- rootVariables.vb
+++ rootVariables.vb
@@ -1,4 +1,7 @@
-﻿Module rootVariables
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Module rootVariables
     Public username As String
     Public admin As Boolean
     Public allowGetdata As Boolean
@@ -8,4 +11,23 @@
     Public allowTugboat As Boolean
     Public lAdd As Boolean
     Public dtTugboat As New DataTable
+
+    Public Function HashPassword(ByVal password As String) As String
+        Using sha256Hash As SHA256 = SHA256.Create()
+            ' Convert the password string to a byte array
+            Dim data As Byte() = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password))
+
+            ' Use a StringBuilder to collect the bytes into a string
+            Dim builder As New StringBuilder()
+
+            For i As Integer = 0 To data.Length - 1
+                builder.Append(data(i).ToString("x2")) ' Convert each byte to a hexadecimal string representation
+            Next
+
+            Return builder.ToString() ' Return the hashed password as a string
+        End Using
+    End Function
+
+
+
 End Module
